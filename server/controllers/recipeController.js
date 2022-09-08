d806231aff768c2ea6048e322e6657cb4894cecd
Nayeem1872 <nayeem.islam.1872@gmail.com>
2022-09-09 05:35:48 +0600

    Updating

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,4 +1,5 @@
-
+require('../models/database')
+const Category =require('../models/Category');
 
 
 
@@ -13,5 +14,64 @@
 
 
 exports.homepage= async(req, res)=>{
-    res.render('index',{title:'Homepage'});
+
+    try{
+
+        res.render('index',{title:'Homepage'});
+
+
+
+    }
+    catch(error){
+        res.status()
+
+
+    }
+
+
+
+
+
+
 }
+
+
+// async function insertDymmCategoryData(){
+
+//     try{
+//         await Category.insertMany([{
+//                     "name": "Thai",
+//                     "image": "thai-food.jpg"
+//                   },
+//                   {
+//                     "name": "American",
+//                     "image": "american-food.jpg"
+//                   }, 
+//                   {
+//                     "name": "Chinese",
+//                     "image": "chinese-food.jpg"
+//                   },
+//                   {
+//                     "name": "Mexican",
+//                     "image": "mexican-food.jpg"
+//                   }, 
+//                   {
+//                     "name": "Indian",
+//                     "image": "indian-food.jpg"
+//                   },
+//                   {
+//                     "name": "Spanish",
+//                     "image": "spanish-food.jpg"
+//                   }]);
+        
+
+//     }
+//     catch(error){
+//         console.log('err',+error)
+
+//     }
+
+
+
+// }
+// insertDymmCategoryData();
